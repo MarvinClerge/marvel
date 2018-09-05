818447e09ccb0074bb0bc25d28519f17ac27b869
Marvin Clerge <clergemarvin@gmail.com>
2018-09-05 13:57:14 -0400

    nav2 implemented, header started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from './components/navbar/NavBar'
+import Header from './components/header/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <NavBar />
+        <Header />
       </div>
     );
   }
--- src/components/navbar/SubNav.js
+++ src/components/navbar/SubNav.js
@@ -10,7 +10,7 @@ class SubNav extends Component {
     this.setState({
       active: event.target.id
     },() => {
-      event.target.classList.add('nav-item-active')
+      event.target.classList.add('sub-nav-item-active')
     })
   }
 
@@ -19,53 +19,66 @@ class SubNav extends Component {
     this.setState({
       active: null
     },() => {
-      event.target.classList.remove('nav-item-active')
+      event.target.classList.remove('sub-nav-item-active')
     })
   }
 
+  renderCategory = () => {
+    if (this.state.active) {
+      return(
+        <div className='sub-nav-category'>
+          <div>
+            {this.state.active}
+          </div>
+        </div>
+      )
+    }
+  }
+
   render(){
-    console.log(this.state);
     return(
       <div className='sub-nav-container'>
         <div className='sub-nav'>
           <ul>
-            <li onMouseEnter={this.setActive}
-              onMouseLeave={this.setInactive} id='vnameeos'>
+            <li onMouseEnter={this.setActive} onMouseLeave={this.setInactive}
+            id='videos' className='sub-nav-item'>
               WATCH VIDEOS
             </li>
 
-            <li onMouseEnter={this.setActive}
-              onMouseLeave={this.setInactive} id='explore'>
+            <li onMouseEnter={this.setActive} onMouseLeave={this.setInactive}
+            id='explore' className='sub-nav-item'>
               EXPLORE THE UNIVERSE
             </li>
 
-            <li onMouseEnter={this.setActive}
-              onMouseLeave={this.setInactive} id='comics'>
+            <li onMouseEnter={this.setActive} onMouseLeave={this.setInactive}
+            id='comics' className='sub-nav-item'>
               READ COMICS
             </li>
 
-            <li onMouseEnter={this.setActive}
-              onMouseLeave={this.setInactive} id='movies'>
+            <li onMouseEnter={this.setActive} onMouseLeave={this.setInactive}
+            id='movies' className='sub-nav-item'>
               MOVIES
             </li>
 
-            <li onMouseEnter={this.setActive}
-              onMouseLeave={this.setInactive} id='shows'>
+            <li onMouseEnter={this.setActive} onMouseLeave={this.setInactive}
+            id='shows' className='sub-nav-item'>
               TV SHOWS
             </li>
 
-            <li onMouseEnter={this.setActive}
-              onMouseLeave={this.setInactive} id='games'>
+            <li onMouseEnter={this.setActive} onMouseLeave={this.setInactive}
+            id='games' className='sub-nav-item'>
               GAMES
             </li>
 
-            <li onMouseEnter={this.setActive}
-              onMouseLeave={this.setInactive} id='more'>
+            <li onMouseEnter={this.setActive} onMouseLeave={this.setInactive}
+            id='more' className='sub-nav-item'>
               MORE
             </li>
-
           </ul>
+
         </div>
+
+        {this.renderCategory()}
       </div>
     )
   }
