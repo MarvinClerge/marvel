60219296c11a105234073f7d886436bb793cac0b
Marvin Clerge <clergemarvin@gmail.com>
2018-09-06 12:55:16 -0400

    header supports images, added to header content

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,19 +5,29 @@ import HeaderControls from './HeaderControls'
 const content = [
   {
     backgroundImage: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avnbpquest_com_mas_dsk_01.jpg",
-    category: "Marvel's Avengers: Black Panther's Quest",
-    title: "Watch the Trailer for the New Season",
+    category: "MARVEL'S AVENGERS: BLACK PANTHER'S QUEST" ,
+    title: "WATCH THE TRAILER FOR THE NEW SEASON",
     description: "The Black Panther begins new adventures on Sunday, September 23 at 9:00PM ET/PT on Disney XD!"
   }, {
     backgroundImage: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/topfemaleduos_com_mas_dsk_01.jpg",
-    category: "Marvel Top Ten",
-    title: "Top 10 Female Duos",
+    category: "MARVEL TOP TEN",
+    title: "TOP 10 FEMALE DUOS",
     description: "From badass besties to super sisters, check out these top team-ups."
   }, {
     backgroundImage: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironfist_com_mas_dsk_01.jpg",
     category: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironfist_lob_log_01_1.png",
-    title: "Season 2 Primer",
+    title: "SEASON 2 PRIMER",
     description: "Get a refresher on Danny Rand and all his friends and foes before the new season premieres this Friday on Netflix!"
+  }, {
+    backgroundImage: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/captainmarvelmag_com_mas_dsk_01.jpg",
+    category: "Marvel Studios",
+    title: "Get Your First Look at Captain Marvel",
+    description: "Entertainment Weekly has your exclusive first look at the badass hero."
+  }, {
+    backgroundImage: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/spidermanps4_com_mas_dsk_01.jpg",
+    category: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/spiderman.png",
+    title: "The Reviews Are In",
+    description: "Here’s what top critics are saying about \"Marvel’s Spider-Man\" for PS4 (don’t worry, we’ll keep it spoiler-free)!"
   }
 ]
 
--- src/components/header/HeaderContent.js
+++ src/components/header/HeaderContent.js
@@ -3,6 +3,24 @@ import './../../css/header.css'
 
 class HeaderContent extends Component {
 
+  renderHeaderCategory = () => {
+    let category = this.props.content.category
+
+    if (category.includes('http')) {
+      return(
+        <div className='header-category-image'>
+          <img src={category} />
+        </div>
+      )
+    } else {
+      return(
+        <p className='header-category'>
+          {category}
+        </p>
+      )
+    }
+  }
+
   render(){
     let {
       backgroundImage,
@@ -22,9 +40,7 @@ class HeaderContent extends Component {
         <div className='header-content'>
           <div className='header-text'>
 
-            <p className='header-category'>
-              {category}
-            </p>
+            {this.renderHeaderCategory()}
 
             <p className='header-title'>
               {title}
@@ -33,7 +49,7 @@ class HeaderContent extends Component {
             <p className='header-description'>
               {description}
             </p>
-            
+
           </div>
         </div>
       </div>
