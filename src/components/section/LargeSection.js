3540dbcac17ff5859c4677a2fbf1fb20a3dafb6c
Marvin Clerge <clergemarvin@gmail.com>
2018-09-06 13:20:42 -0400

    largeSection and smallSection components added

---
+++ src/components/section/LargeSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './../../css/section.css'
+
+const LargeSection = (props) => {
+  return(
+    <div className='large-section-container'>
+      <div className='large-section'>
+
+      </div>
+    </div>
+  )
+}
+
+export default LargeSection;
---
+++ src/components/section/SmallSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './../../css/section.css'
+
+const SmallSection = (props) => {
+  return(
+    <div className='small-section-container'>
+      <div className='small-section'>
+
+      </div>
+    </div>
+  )
+}
+
+export default SmallSection;
