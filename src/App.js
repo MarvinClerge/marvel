ac5bde3d892df5a404e7b6e8cadfbf56ac8d43aa
Marvin Clerge <clergemarvin@gmail.com>
2018-09-06 13:59:12 -0400

    large section main article added

--- src/App.js
+++ src/App.js
@@ -10,19 +10,23 @@ const largeSectionContent = {
   content: [
     {
       category: "MOVIES",
+      image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/cm_card.jpg",
       title: "Get Your First Look at Marvel Studios' 'Captain Marvel'",
       description: "Pick up your copy of Entertainment Weekly on newsstands this Friday!",
       timeAgo: "1 DAY AGO"
     }, {
       category: "COMICS",
+      image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/cover._sx1280_ql80_ttd__0.jpg",
       title: "Captain Marvel Essential Reading",
       timeAgo: "5 MONTHS AGO"
     }, {
       category: "MOVIES",
+      image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/cm_sop_photo_better.jpg",
       title: "Production Underway on Marvel Studios’ ‘Captain Marvel’",
       timeAgo: "5 MONTHS AGO"
     }, {
       category: "CULTURE & LIFESTYLE",
+      image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/craft_card.jpg",
       title: "Show Off Your Super Fandom Like Kamala Khan with This DIY Captain Marvel Notebook",
       timeAgo: "1 WEEK AGO"
     }
--- src/components/section/LargeSection.js
+++ src/components/section/LargeSection.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './../../css/section.css'
 
 const LargeSection = (props) => {
+
+  const main = props.content[0]
+
   return(
     <div className='large-section-container'>
       <div className='large-section'>
-        
+
+        <p className='large-section-title'>
+          {props.title}
+        </p>
+
+        <div className='large-section-main'>
+          <div className='large-section-main-image'>
+            <img src={main.image} />
+          </div>
+
+          <div className='large-section-main-content'>
+            <p className='large-section-content-text'>
+              {main.category}
+            </p>
+            <p className='large-section-content-title'>
+              {main.title}
+            </p>
+            <p className='large-section-content-text'>
+              {main.description}
+            </p>
+            <p className='large-section-content-text'>
+              {main.timeAgo}
+            </p>
+          </div>
+        </div>
+
       </div>
     </div>
   )
